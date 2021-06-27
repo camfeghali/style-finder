28d184a8e2e76ee676ce704b6d1ca139906de149
Camille Feghali <camfeg@gmail.com>
2021-06-27 14:11:32 +0200

    added toast

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <template>
   <div>
-    <InputText />
-    <Button label="Greet"></Button>
+    <InputText v-model="text" />
+    <Button label="Greet" icon="pi pi-user" @click="greet"></Button>
+    <Toast />
   </div>
 </template>
 
-<script></script>
+<script>
+import { defineComponent, ref } from "@vue/runtime-core";
+import { useToast } from "primevue/usetoast";
+
+export default defineComponent({
+  setup() {
+    const toast = useToast();
+    const text = ref("Prime");
+
+    const greet = () => {
+      console.log("greeting!");
+      toast.add({
+        severity: "info",
+        summary: "hello",
+        detail: text,
+      });
+    };
+
+    return {
+      text,
+      greet,
+    };
+  },
+});
+// export default {
+//   data() {
+//     return {
+//       text: null,
+//     };
+//   },
+//   methods: {
+//     greet() {
+//       console.log("firing!");
+//       console.log("this.$toast", this.$toast);
+//       this.$toast.add({
+//         severity: "info",
+//         summary: "hello",
+//         detail: this.text,
+//       });
+//     },
+//   },
+// };
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
 
 import "primevue/resources/themes/nova-vue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
@@ -12,8 +14,10 @@ import "primeicons/primeicons.css"; //icons
 const app = createApp(App);
 
 app.use(PrimeVue);
+app.use(ToastService);
 
 app.component("Button", Button);
 app.component("InputText", InputText);
+app.component("Toast", Toast);
 
 app.mount("#app");
